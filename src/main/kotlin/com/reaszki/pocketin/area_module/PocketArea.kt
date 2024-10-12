f5a765b0457e43ee9fdebb812bf30fa13f7ecdb7
reis <armond.reaszki@gmail.com>
2024-10-12 06:27:27 -0300

    "Intfication" of some methods

--- src/main/kotlin/com/reaszki/pocketin/area_module/PocketArea.kt
+++ src/main/kotlin/com/reaszki/pocketin/area_module/PocketArea.kt
@@ -11,26 +11,22 @@ object PocketArea {
 
     var loadedAreas = mutableListOf<PocketRegion>()
 
-    fun createArea(
-        name: String,
-        world: String,
-        pos1: List<Long>,
-        pos2: List<Long>
-    ) {
+    fun createArea(name: String, world: String, pos1: List<Int>, pos2: List<Int>) {
         var area = PocketRegion(
             name,
             world,
-            mutableListOf(pos1[0].toDouble(), pos1[1].toDouble(), pos1[2].toDouble()),
-            mutableListOf(pos2[0].toDouble(), pos2[1].toDouble(), pos2[2].toDouble())
+            pos1,
+            pos2
         )
 
         saveArea(area)
     }
 
-    fun saveArea(area: PocketRegion) {
+    private fun saveArea(area: PocketRegion) {
         if (Files.lookForSubFolder(Plugin.plugin, "areas")) {
             var pocketRegionJson = Gson().toJson(area)
             var file = File(Files.returnFolder(Plugin.plugin, "areas"), "${area.name}.json")
+
             file.createNewFile()
             file.writeText(pocketRegionJson)
 
@@ -41,12 +37,12 @@ object PocketArea {
     fun loadAreas() {
         if (Files.lookForSubFolder(Plugin.plugin, "areas")) {
             var folder = Files.returnFolder(Plugin.plugin, "areas")
+
             folder.listFiles().forEach { file ->
                 loadedAreas.add(Gson().fromJson(file.readText(), PocketRegion::class.java))
             }
         }
     }
-
 
     fun lookUpArea(pos: Location): MutableList<PocketRegion?> {
         val listOfAreas = mutableListOf<PocketRegion?>()
@@ -56,18 +52,18 @@ object PocketArea {
         val areaZ = pos.z.toInt()
 
         loadedAreas.forEach { area ->
-            val area1X = minOf(area.pos1[0], area.pos2[0]).toInt()
-            val area1Y = minOf(area.pos1[1], area.pos2[1]).toInt()
-            val area1Z = minOf(area.pos1[2], area.pos2[2]).toInt()
+            val area1X = minOf(area.pos1[0], area.pos2[0])
+            val area1Y = minOf(area.pos1[1], area.pos2[1])
+            val area1Z = minOf(area.pos1[2], area.pos2[2])
 
-            val area2X = maxOf(area.pos1[0], area.pos2[0]).toInt()
-            val area2Y = maxOf(area.pos1[1], area.pos2[1]).toInt()
-            val area2Z = maxOf(area.pos1[2], area.pos2[2]).toInt()
+            val area2X = maxOf(area.pos1[0], area.pos2[0])
+            val area2Y = maxOf(area.pos1[1], area.pos2[1])
+            val area2Z = maxOf(area.pos1[2], area.pos2[2])
 
             if (
-                areaX >= area1X && areaX <= area2X &&
-                areaY >= area1Y && areaY <= area2Y &&
-                areaZ >= area1Z && areaZ <= area2Z
+                areaX in (area1X..area2X) &&
+                areaY in (area1Y..area2Y) &&
+                areaZ in (area1Z..area2Z)
             ) {
                 listOfAreas.add(area)
             }
