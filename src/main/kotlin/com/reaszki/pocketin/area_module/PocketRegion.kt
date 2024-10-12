e9e9df394a4f4d33b523cc27f2c3271fc7ef3438
reis <armond.reaszki@gmail.com>
2024-10-12 06:28:31 -0300

    MutableList<Double> to List<Int>

--- src/main/kotlin/com/reaszki/pocketin/area_module/PocketRegion.kt
+++ src/main/kotlin/com/reaszki/pocketin/area_module/PocketRegion.kt
@@ -3,6 +3,6 @@ package com.reaszki.pocketin.area_module
 data class PocketRegion(
     val name: String,
     val world: String,
-    val pos1: MutableList<Double>,
-    val pos2: MutableList<Double>
+    val pos1: List<Int>,
+    val pos2: List<Int>
 )
