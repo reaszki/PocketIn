e258d032d28518790429380285818c9feb6fae54
reis <armond.reaszki@gmail.com>
2024-10-12 06:29:18 -0300

    Object to PocketWand related things

--- src/main/kotlin/com/reaszki/pocketin/area_module/PocketWand.kt
+++ src/main/kotlin/com/reaszki/pocketin/area_module/PocketWand.kt
@@ -1,20 +1,58 @@
 package com.reaszki.pocketin.area_module
 
 import com.reaszki.pocketin.config_module.NBT
+import de.tr7zw.nbtapi.NBTType
 import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
 
-fun PocketWand(): ItemStack {
-    var item = ItemStack(Material.STICK)
-    item = NBT.setDisplayName(item, "「 Area Wand 」")
-    item = NBT.setLore(item, listOf("§8Left click §7pos1§8.", "§8Right click §7pos2§8."))
-    NBT.createPocketInCompound(item)
+object PocketWand {
+    fun item(): ItemStack {
+        var item = ItemStack(Material.STICK)
+        item = NBT.setDisplayName(item, "「 Area Wand 」")
+        item = NBT.setLore(item, listOf("§8Left click §7pos1§8.", "§8Right click §7pos2§8."))
+        NBT.createPocketInCompound(item)
 
-    return item
+        return item
+    }
+
+    // TODO: ARRUMAR BAGUNÇA COM ARRAY
+    fun PocketWandSelection(event: PlayerInteractEvent) {
+        var hand = event.player.inventory.itemInMainHand
+
+        if (hand.type == Material.AIR) return
+
+        var correctItem = false
+        de.tr7zw.nbtapi.NBT.get(hand) { nbt ->
+            if (nbt.hasTag("pocketin", NBTType.NBTTagCompound)) correctItem = true
+        }
+        if (!correctItem) return
+        when (event.action) {
+            Action.LEFT_CLICK_BLOCK -> {
+                var pos1 = event.clickedBlock
+                var arr = intArrayOf(pos1!!.x, pos1!!.y, pos1!!.z)
+                de.tr7zw.nbtapi.NBT.modify(hand) { nbt ->
+                    nbt.getCompound("pocketin")?.setIntArray("pos1", arr)
+                }
+            }
+            Action.RIGHT_CLICK_BLOCK -> {
+                if (event.hand != EquipmentSlot.HAND) return
+                var pos2 = event.clickedBlock
+                var arr = intArrayOf(pos2!!.x, pos2!!.y, pos2!!.z)
+                de.tr7zw.nbtapi.NBT.modify(hand) { nbt ->
+                    nbt.getCompound("pocketin")?.setIntArray("pos2", arr)
+                }
+            }
+            else -> {}
+        }
+        event.isCancelled = true
+    }
 }
 
 class WandConsumeCommand: CommandExecutor {
@@ -22,10 +60,16 @@ class WandConsumeCommand: CommandExecutor {
         if (sender !is Player) return false
         if (!NBT.lookForCompound(sender.inventory.itemInMainHand, "pocketin")) return false
         if (args.isEmpty()) return false
-        PocketArea.createArea(args[0], sender.world.name, NBT.retriveWandPos(sender.inventory.itemInMainHand,"pos1"), NBT.retriveWandPos(sender.inventory.itemInMainHand,"pos2"))
+
+        PocketArea.createArea(
+            args[0], sender.world.name,
+            NBT.retriveWandPos(sender.inventory.itemInMainHand,"pos1"),
+            NBT.retriveWandPos(sender.inventory.itemInMainHand,"pos2")
+        )
         sender.inventory.itemInMainHand.amount = 0
         sender.sendMessage("§aArea created (${args[0]})!")
 
         return true
     }
 }
+
